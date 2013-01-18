309740c4c6a5b412d3056813c1839e6c228db7e7
Evan Cummings <evan.cummings@umontana.edu>
2013-01-17 18:37:13 -0700

    new directory

--- fmicData.py
+++ fmicData.py
@@ -133,7 +133,7 @@ class FmicData():
     rho815 = vstack((t, age815, z815))
     por    = vstack((t, por815, porAll))
     
-    directory = 'data/fmic/CummingsExperiment2_' + exp + '/'
+    directory = 'data/fmic/CummingsExperiment' + exp + '/'
   
     savetxt(directory + 'CummingsExperiment' + exp + 'Age.txt',         a,
             delimiter='\t')
